b9b232b1ea2b417a3f92f8444dd6762904771c9b
Hannah Lee and Katie Fujihara <hannah@>
2018-01-09 09:56:13 -0800

    Add and remove background color effects.

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,23 @@ $(document).ready(function() {
   $(".clickable").click(function() {
     $("#oxygen-showing").toggle();
     $("#oxygen-hidden").toggle();
+    $("#oxygen-showing").removeClass();
+    $("#oxygen-showing").addClass("green-background");
   });
 });
 
 $(document).ready(function() {
   $(".hover").hover(function() {
     $("#flotation-showing").fadeToggle();
+    $("#flotation-showing").removeClass();
+    $("#flotation-showing").addClass("red-background");
   });
 });
 
 $(document).ready(function() {
   $(".slidetoggle").hover(function() {
     $("#evacuation-showing").slideToggle();
+    $("#evacuation-showing").removeClass();
+    $("#evacuation-showing").addClass("blue-background");
   });
 });
