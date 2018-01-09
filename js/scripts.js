106c5f6b2531ff9488b1f5ea3ba0dc42c6f2988e
Hannah Lee and Katie Fujihara <hannah@>
2018-01-09 09:30:26 -0800

    Add images, jQuery, effects, and css styles.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,18 @@
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#oxygen-showing").toggle();
+    $("#oxygen-hidden").toggle();
+  });
+});
 
+$(document).ready(function() {
+  $(".hover").hover(function() {
+    $("#flotation-showing").fadeToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".slidetoggle").hover(function() {
+    $("#evacuation-showing").slideToggle();
+  });
+});
